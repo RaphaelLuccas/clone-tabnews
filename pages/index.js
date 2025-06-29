24eb48f07198c9866523e98e5f79ac8f760333df
raphaelluccasdev <raphaelluccasdev@gmail.com>
2025-06-29 08:29:25 -0300

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Teste Teste Teste</h1>
+  return <h1>Teste Teste Teste</h1>;
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("Teste 2 de identação");
 }
 
 export default Home;
